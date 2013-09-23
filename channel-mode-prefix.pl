bb547fb5224d0b6b52be86f35305463287a8f447
Farow <farow_spam@lavabit.com>
2013-09-23 23:32:47 +0300

    Changes to avoid crashing
    The script would die and crash HexChat when joining a channel that has
    no current tab (or focus).
    
    - Removed channel checking in `generic_mode_change`
    - Add a way to pass context to `set_prefix`

--- channel-mode-prefix.pl
+++ channel-mode-prefix.pl
@@ -1,7 +1,7 @@
 use common::sense;
 use Xchat;
 
-Xchat::register 'Channel mode prefix', '1.01', 'Adds your mode symbol at the beginning of the channel name.';
+Xchat::register 'Channel mode prefix', '1.02', 'Adds your mode symbol at the beginning of the channel name.';
 
 Xchat::hook_print 'Channel Mode Generic', \&generic_mode_change;
 Xchat::hook_print $_, \&mode_change, { 'data' => $_ }
@@ -10,8 +10,6 @@ Xchat::hook_print $_, \&mode_change, { 'data' => $_ }
 		'Channel Operator',      'Channel DeOp';
 
 Xchat::hook_server '353', \&channel_join;
-
-#set_prefix();
 
 sub generic_mode_change {
 	my ($data) = @_;
@@ -23,9 +21,8 @@ sub generic_mode_change {
 	my $current_nick     = Xchat::get_info 'nick';
 
 	return Xchat::EAT_NONE if fc $current_nick ne fc $nick;
-	return Xchat::EAT_NONE if $channel !~ /^#/;
 
-	Xchat::hook_timer 0, \&set_prefix;
+	delay(\&set_prefix);
 
 	return Xchat::EAT_NONE;
 }
@@ -36,30 +33,45 @@ sub mode_change {
 
 	return Xchat::EAT_NONE if fc $nick ne fc $data->[1];
 
-	Xchat::hook_timer 0, \&set_prefix;
+	delay(\&set_prefix);
 
 	return Xchat::EAT_NONE;
 }
 
 sub channel_join {
-	my $users = substr $_[1][5], 1;
-	my $nick  = Xchat::get_info 'nick';
+	my $channel = $_[0][4];
+	my $users   = substr $_[1][5], 1;
+	my $nick    = Xchat::get_info 'nick';
 
-	Xchat::hook_timer 0, \&set_prefix if $users =~ /.\Q$nick\E/;
+	delay(\&set_prefix, $channel) if $users =~ /^.\Q$nick\E$/;
 
 	return Xchat::EAT_NONE;
 }
 
 sub set_prefix {
+	my $context = shift // Xchat::get_context;
+	Xchat::set_context $context;
+	my $info    = Xchat::context_info;
+
+	#make sure it's a channel tab
+	return 0 if $info->{'type'} != 2;
+
+	#get the channel mode prefix and channel tab name
 	my $prefix = Xchat::user_info->{'prefix'};
+	my $tab    = $info->{'channel'};
 
-	return Xchat::REMOVE if !defined $prefix;
+	Xchat::command "settab $prefix$tab";
+	return 1;
+}
 
-	my $context = Xchat::get_context;
-	my ($tab)   = grep { $_->{'context'} == $context && $_->{'type'} == 2 } Xchat::get_list 'channels';
+sub delay {
+	my ($code, @params) = @_;
 
-	return Xchat::REMOVE if !defined $tab;
+	Xchat::hook_timer 0, sub {
+		$code->(@params);
 
-	Xchat::command "settab $prefix$tab->{'channel'}";
-	return Xchat::REMOVE;
+		return Xchat::REMOVE;
+	};
+
+	return 1;
 }
